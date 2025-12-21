56da18599b5de571b2034290aeafecf7c1d4d004
Jean <orangedrum@users.noreply.github.com>
2025-12-21 13:48:08 -0500

    Create builder-registry.ts to make "guest list" of react components

---
+++ components/builder-registry.ts
@@ -0,0 +1,6 @@
+"use client";
+import { Builder } from "@builder.io/react";
+
+// This is where we will import all our custom components to register them
+// import "@/components/MyCustomComponent";
+import "@/components/AuthForm"; 
