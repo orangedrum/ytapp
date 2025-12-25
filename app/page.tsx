e96e239cd7d1d6ccfbf126257303be2812b141b7
Jean Kaluza <orangedrum@Jeans-MacBook-Pro-2.local>
2025-12-25 18:10:01 -0500

    removing confusing catch-all page

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,18 @@ builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 interface PageProps {
   params?: {
-    page: string[];
+    // This page is for the root, so it won't have params
   };
 }
 
 export default async function Page(props: PageProps) {
-  // If props.params.page is undefined, we are on the homepage ('/')
-  const urlPath = '/' + (props.params?.page?.join('/') || '');
-
   const content = await builder
     // Get the page content from Builder with the specified URL
     .get('page', {
-      userAttributes: {
-        // Combine the array of path segments into a single URL string
-        // e.g. ['tango', 'boleos'] becomes '/tango/boleos'
-        urlPath,
-      },
+      userAttributes: { urlPath: '/' },
     })
     // Convert the result to a promise
     .toPromise();
-
-  // --- DIAGNOSTIC LOG ---
-  // This will print to the Vercel logs so we can see exactly what Builder is sending back
-  console.log('Builder content for', urlPath, ':', content);
 
   return (
     <>
