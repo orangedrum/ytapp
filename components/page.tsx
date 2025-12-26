f416fb16c03f3551a07dd6d07915ef9c905dffb0
Jean Kaluza <orangedrum@Jeans-MacBook-Pro-2.local>
2025-12-25 19:05:40 -0500

    removing greed catch-all route

---
+++ components/page.tsx
@@ -0,0 +1,11 @@
+import TypographyGuide from "@/components/TypographyGuide";
+
+export default function StyleguidePage() {
+  return (
+    <main className="py-12">
+      <div className="container mx-auto">
+        <TypographyGuide />
+      </div>
+    </main>
+  );
+}
