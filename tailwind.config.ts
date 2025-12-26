e53fd2e866432d5697c5042cbb17297133fa9a9d
Jean Kaluza <orangedrum@Jeans-MacBook-Pro-2.local>
2025-12-25 21:00:47 -0500

    fixing tailwind styling issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./{app,components}/**/*.{js,ts,jsx,tsx,mdx}", // This line is the critical change
   ],
   prefix: "",
   theme: {
